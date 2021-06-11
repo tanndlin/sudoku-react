960e34ece804b5c44590695437514b674170375e
Tanndlin <tanndlin@gmail.com>
2021-06-11 11:30:58 -0500

    Create Grid

---
+++ src/Container.js
@@ -0,0 +1,109 @@
+const Container = () => {
+  return (
+    <div className="container">
+      <div className="gameContainer">
+        <div className="gameBlock">
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+        </div>
+        <div className="gameBlock">
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+        </div>
+        <div className="gameBlock">
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+        </div>
+        <div className="gameBlock">
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+        </div>
+        <div className="gameBlock">
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+        </div>
+        <div className="gameBlock">
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+        </div>
+        <div className="gameBlock">
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+        </div>
+        <div className="gameBlock">
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+        </div>
+        <div className="gameBlock">
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+          <div className="gameCell"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Container;
---
+++ src/Topbar.js
@@ -0,0 +1,11 @@
+const Topbar = () => {
+  const score = 0;
+
+  return (
+    <nav className="topbar">
+      <h1>Score: {score}</h1>
+    </nav>
+  );
+};
+
+export default Topbar;

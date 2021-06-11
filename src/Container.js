8998c62881b51b259a3c22f661af573ee6a39afa
Tanndlin <tanndlin@gmail.com>
2021-06-11 12:30:05 -0500

    On Key Down Events

--- src/Container.js
+++ src/Container.js
@@ -1,105 +1,525 @@
 const Container = () => {
+  const handleCellClick = (e) => {
+    e.target.classList.add('highlighted');
+  };
+
+  const handleKey = (e) => {
+    console.log(e.key);
+    if (!parseInt(e.key)) return;
+  };
+
   return (
-    <div className="container">
+    <div
+      className="container"
+      onKeyDown={(e) => {
+        handleKey(e);
+      }}
+      tabIndex="0"
+    >
       <div className="gameContainer">
         <div className="gameBlock">
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
         </div>
         <div className="gameBlock">
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
         </div>
         <div className="gameBlock">
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
         </div>
         <div className="gameBlock">
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
         </div>
         <div className="gameBlock">
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
         </div>
         <div className="gameBlock">
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
         </div>
         <div className="gameBlock">
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
         </div>
         <div className="gameBlock">
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
         </div>
         <div className="gameBlock">
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
-          <div className="gameCell"></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
+          <div
+            className="gameCell"
+            onClick={(e) => {
+              handleCellClick(e);
+            }}
+          ></div>
         </div>
       </div>
     </div>

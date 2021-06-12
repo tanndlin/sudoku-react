a754ba010aee333494b9dc11aef83c872fbc35ec
Tanndlin <tanndlin@gmail.com>
2021-06-11 21:22:21 -0500

    Created Sudoku Generator

--- src/Topbar.js
+++ src/Topbar.js
@@ -1,9 +1,52 @@
+import Sudoku from './sudoku';
+
 const Topbar = () => {
   const score = 0;
+  const fillRate = 0.3;
+  const board = new Sudoku();
+
+  const setCell = (valueObj) => {
+    const cells = Array.from(document.getElementsByClassName('gameCell'));
+    cells.forEach((c) => {
+      const [, id] = c.id.split(' ');
+      if (parseInt(id) !== valueObj.index) {
+        return;
+      }
+
+      if (valueObj.value === 0) {
+        c.innerHTML = '';
+        c.classList.remove('hard');
+        return;
+      }
+
+      c.innerHTML = valueObj.value;
+      c.classList.add('hard');
+    });
+  };
+
+  const generateSudoku = () => {
+    board.createNewBoard();
+    board.generateBoard();
+    const sequence = board.getInputSequence(fillRate).map((value, i) => {
+      return { index: i, value: value };
+    });
+
+    const interval = setInterval(() => {
+      const index = Math.floor(Math.random() * sequence.length);
+      setCell(sequence[index]);
+      sequence.splice(index, 1);
+
+      if (sequence.length === 0) {
+        clearInterval(interval);
+        console.log('Done');
+      }
+    }, 10);
+  };
 
   return (
     <nav className="topbar">
-      <h1>Score: {score}</h1>
+      <h1 id="score">Score: {score}</h1>
+      <button onClick={generateSudoku}>Generate Sudoku</button>
     </nav>
   );
 };
---
+++ src/sudoku.js
@@ -0,0 +1,117 @@
+class Sudoku {
+  constructor() {
+    // this.createNewBoard();
+  }
+
+  createNewBoard() {
+    this.index = 0;
+    this.board = [];
+    for (let i = 0; i < 9; i++) {
+      const row = [];
+      for (let j = 0; j < 9; j++) {
+        row.push(0);
+      }
+      this.board.push(row);
+    }
+    this.possible = this.board.map((row) => row.map((_) => [1, 2, 3, 4, 5, 6, 7, 8, 9]));
+  }
+
+  generateBoard() {
+    if (this.checkFilled()) {
+      return;
+    }
+
+    this.populateBoard();
+    this.generateBoard();
+  }
+
+  backtrack() {
+    const x = this.index % 9;
+    const y = Math.floor(this.index / 9);
+    this.possible[y][x] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    this.board[y][x] = 0;
+    this.index--;
+  }
+
+  populateBoard() {
+    const x = this.index % 9;
+    const y = Math.floor(this.index / 9);
+    const possibleValues = this.possible[y][x];
+
+    if (!possibleValues.length) {
+      this.backtrack();
+      return this.populateBoard();
+    }
+
+    const index = Math.floor(Math.random() * possibleValues.length);
+    const val = possibleValues[index];
+    if (!this.isValid(val, x, y)) {
+      this.possible[y][x].splice(index, 1);
+      return this.populateBoard();
+    }
+    this.board[y][x] = val;
+    this.possible[y][x].splice(index, 1);
+    this.index++;
+  }
+
+  isValid(val, x, y) {
+    const neighbors = [];
+    for (let i = 0; i < 9; i++) {
+      if (i === x) {
+        continue;
+      }
+      neighbors.push(this.board[y][i]);
+    }
+
+    for (let j = 0; j < 9; j++) {
+      if (j === y) {
+        continue;
+      }
+      neighbors.push(this.board[j][x]);
+    }
+
+    const blockX = Math.floor(x / 3) * 3;
+    const blockY = Math.floor(y / 3) * 3;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const newX = blockX + i;
+        const newY = blockY + j;
+        if (newX === x && newY === y) {
+          continue;
+        }
+
+        neighbors.push(this.board[newY][newX]);
+      }
+    }
+
+    return !neighbors.includes(val);
+  }
+
+  generateSudokuList() {
+    const ret = [];
+    this.board.forEach((row) => ret.push(...row));
+    return ret;
+  }
+
+  getInputSequence(fillRate) {
+    const ret = [];
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (Math.random() > fillRate) {
+          ret.push(0);
+          continue;
+        }
+
+        ret.push(this.board[j][i]);
+      }
+    }
+
+    return ret;
+  }
+
+  checkFilled() {
+    return !this.board.some((row) => row.includes(0));
+  }
+}
+
+export default Sudoku;
